578815eb40c0c7d07ccfae41ea9e52eb2819de7e
Sydney Holland <sydneymh@udel.edu>
2024-03-12 23:36:39 -0400

    finished three of the functions

--- src/bad-components/ChooseTeam.tsx
+++ src/bad-components/ChooseTeam.tsx
@@ -11,17 +11,13 @@ const PEOPLE = [
 ];
 
 export function ChooseTeam(): JSX.Element {
-    const [allOptions, setAllOptions] = useState<string[]>(PEOPLE);
+    const [allOptions] = useState<string[]>(PEOPLE);
     const [team, setTeam] = useState<string[]>([]);
-    const [newMember, setMember] = useState<string>(PEOPLE[0]);
-    const [memberIndex, setIndex] = useState<number>(0);
 
-    function chooseMember() {
-        if (!team.includes(allOptions[memberIndex])) {
-            const newTeam = [...team, newMember];
+    function chooseMember(option: string) {
+        if (!team.includes(option)) {
+            const newTeam = [...team, option];
             setTeam(newTeam);
-        } else {
-            setIndex(memberIndex + 1);
         }
     }
 
@@ -37,7 +33,10 @@ export function ChooseTeam(): JSX.Element {
                     {allOptions.map((option: string) => (
                         <div key={option} style={{ marginBottom: "4px" }}>
                             Add{" "}
-                            <Button onClick={chooseMember} size="sm">
+                            <Button
+                                onClick={() => chooseMember(option)}
+                                size="sm"
+                            >
                                 {option}
                             </Button>
                         </div>
